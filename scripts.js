e0707dd17230b5cf2a37404ce453eaa32b4e4473
mugenfuko <sebastiaan.hopmans@gmail.com>
2020-05-19 15:17:33 +0200

    Uploaded the files of the assignment

---
+++ scripts.js
@@ -0,0 +1,31 @@
+const container = document.querySelector('#container');
+
+const content = document.createElement('div');
+content.classList.add('content');
+content.textContent = 'This is the glorious text-content!';
+container.appendChild(content);
+
+// Adding a red paragraph
+const pRed = document.createElement('p');
+pRed.style.color = 'red';
+pRed.textContent = 'Hey I\'m red!';
+container.appendChild(pRed);
+
+// Adding a blue h3
+const h3Blue = document.createElement('h3');
+h3Blue.style.color = 'blue';
+h3Blue.textContent = 'I\'m a blue h3!';
+container.appendChild(h3Blue);
+
+// Adding a div with black border and pink backgroundColor
+const div = document.createElement('div');
+div.style.cssText =
+'border-style: solid; border-color: black; background-color: pink';
+// Adding a h1 and p into this div
+const h1 = document.createElement('h1');
+h1.textContent = 'I\'m in a div';
+const p = document.createElement('p');
+p.textContent = 'ME TOO!';
+div.appendChild(h1);
+div.appendChild(p);
+container.appendChild(div);
